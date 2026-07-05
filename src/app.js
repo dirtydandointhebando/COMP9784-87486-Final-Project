eda4e7435a8c198035d10cdd589b10403fcb2c0f
Aidan <aidandepaul@gmail.com>
2026-07-05 10:20:17 -0400

    Add compression

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import helmet from "helmet";
 
+import compression from "compression";
+
 import licensePlateRoutes from "./routes/licensePlateRoutes.js";
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use(helmet());
+
+app.use(compression());
 
 app.use("/license-plates", licensePlateRoutes);
 
