d64ba07d7920123e62ae8c82f7b127ea61aac5de
Aidan <aidandepaul@gmail.com>
2026-07-05 10:21:42 -0400

    Add rate limiter

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from "helmet";
 
 import compression from "compression";
 
+import rateLimit from "express-rate-limit";
+
 import licensePlateRoutes from "./routes/licensePlateRoutes.js";
 
 const app = express();
@@ -13,6 +15,13 @@ app.use(express.json());
 app.use(helmet());
 
 app.use(compression());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+
+app.use(limiter);
 
 app.use("/license-plates", licensePlateRoutes);
 
