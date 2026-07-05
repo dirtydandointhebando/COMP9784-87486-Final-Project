2cadacf639d19d04915fbac783c11abc1b4fa372
Aidan <aidandepaul@gmail.com>
2026-07-04 23:04:28 -0400

    Add assign license plate to controller

--- src/controllers/licensePlateController.js
+++ src/controllers/licensePlateController.js
@@ -1,7 +1,17 @@
-import { getStatus } from "../services/licensePlateService.js";
+import { getStatus, assignPlate } from "../services/licensePlateService.js";
 
 export const getApiStatus = (req, res) => {
     const status = getStatus();
 
     res.status(200).json(status);
+};
+
+export const assignLicensePlate = async (req, res) => {
+
+    const { vin } = req.params;
+
+    const result = await assignPlate(vin);
+
+    res.status(result.status).json(result);
+
 };
