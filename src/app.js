f262b31ac3cd952d0663ab5bfae1b332e60f114d
Aidan <aidandepaul@gmail.com>
2026-07-05 10:19:22 -0400

    Add helmet

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 
+import helmet from "helmet";
+
 import licensePlateRoutes from "./routes/licensePlateRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(helmet());
 
 app.use("/license-plates", licensePlateRoutes);
 
